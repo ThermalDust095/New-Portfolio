c6d4652ba1dbebf635bddfebdab6d8e4b4c13f3a
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2024-03-26 13:29:38 +0530

    disgusting mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,29 @@ import { motion } from 'framer-motion'
 
 export default function Navbar(props) {
 
-  const [dl, setdl] = useState({})
+  const [mWindow, setMwindow] = useState(false)
+  const [mnav, setmnav] = useState(false)
+
   const navbarRef = useRef(null)
+  const itemsRef = useRef(null)
+  
+  function handleMclick(){
+
+    setmnav(!mnav)
+
+    if(mnav){
+      itemsRef.current.style = "display: flex"
+      navbarRef.current.style = "height: 100vh"
+      // navbarRef.current.style = "background: #010410"
+    }
+
+    if(!mnav){
+      itemsRef.current.style = "display: none"
+      navbarRef.current.style = "height: 17.749vh"
+      // navbarRef.current.style = "background: transparent"
+    }
+
+  }
   
   useEffect(() => {
     if(props.page != null){
@@ -15,8 +36,11 @@ export default function Navbar(props) {
       elem.style.color = "#0ff"
     }
 
+    if(window.innerWidth <= 768){
+      setMwindow(true)
+    }
+
     if(props.delay){
-      setdl({delay: "3s"})
       navbarRef.current.style = "position:absolute";
 
     }
@@ -25,8 +49,22 @@ export default function Navbar(props) {
 
   },[])
 
+  if(mWindow){
+    return(
+      <div div id='navbar' ref={navbarRef} style={{zIndex: 5}}>
+        <h1 id='name' onClick={(e) => handleMclick(e)}>Akhil Pendyala</h1>
+        <ul id='items' ref={itemsRef}>
+                <li id='home'><Link to="/">Home</Link></li>
+                <li id='about'><Link to="/about">About</Link></li>
+                <li id='projects'><Link to="/projects">Projects</Link></li>
+                <li id='artwork'><Link to="/">Artwork</Link></li>
+        </ul>
+      </div>
+    )
+  }
+
   return (
-    <motion.div id='navbar' ref={navbarRef} initial={{opacity: 0}} animate={{opacity:1}} transition={dl}>
+    <motion.div id='navbar' ref={navbarRef} initial={{opacity: 0}} animate={{opacity:1}}>
             <h1 id='name'><Link to="/">Akhil Pendyala</Link></h1>
             <ul id='items'>
                 <li id='about'><Link to="/about">About</Link></li>
